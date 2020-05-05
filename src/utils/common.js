309585b24d8ba384b1dd97f6c8a601ba945babe0
ivlev <ivlev.me@gmail.com>
2020-05-05 19:56:23 +1000

    Task 7.1

--- src/utils/common.js
+++ src/utils/common.js
@@ -9,5 +9,19 @@ const formatDate = (date) => {
   return moment(date).format(`DD MMMM`);
 };
 
+const isRepeating = (repeatingDays) => {
+  return Object.values(repeatingDays).some(Boolean);
+};
 
-export {formatTime, formatDate};
+const isOverdueDate = (dueDate, date) => {
+  return dueDate < date && !isOneDay(date, dueDate);
+};
+
+const isOneDay = (dateA, dateB) => {
+  const a = moment(dateA);
+  const b = moment(dateB);
+  return a.diff(b, `days`) === 0 && dateA.getDate() === dateB.getDate();
+};
+
+
+export {formatTime, formatDate, isRepeating, isOverdueDate, isOneDay};
